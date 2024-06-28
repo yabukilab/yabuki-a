32e6b98b3bef7784a3408579a5b49911340828bb
2242120 <s2242120ub@s.chibakoudai.jp>
2024-06-28 15:46:21 +0900

    a

--- htdocs/add_user.php
+++ htdocs/add_user.php
@@ -1,17 +1,29 @@
 <?php
-// データベース接続情報
-$servername = "localhost";
-$username = "testuser";
-$password = "pass"; // データベースパスワード
-$dbname = "mydb";
-
-// データベース接続の確立
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// 接続を確認
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+# HTMLでのエスケープ処理をする関数（データベースとは無関係だが，ついでにここで定義しておく．）
+function h($var) {
+    if (is_array($var)) {
+      return array_map('h', $var);
+    } else {
+      return htmlspecialchars($var, ENT_QUOTES, 'UTF-8');
+    }
+  }
+  
+  $dbServer = isset($_ENV['MYSQL_SERVER'])    ? $_ENV['MYSQL_SERVER']      : '127.0.0.1';
+  $dbUser = isset($_SERVER['MYSQL_USER'])     ? $_SERVER['MYSQL_USER']     : 'testuser';
+  $dbPass = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : 'pass';
+  $dbName = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'mydb';
+  
+  $dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
+  
+  try {
+    $db = new PDO($dsn, $dbUser, $dbPass);
+    # プリペアドステートメントのエミュレーションを無効にする．
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    # エラー→例外
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch (PDOException $e) {
+    echo "Can't connect to the database: " . h($e->getMessage());
+  }
 
 // フォームからの入力を取得
 $user = $_POST['username'] ?? '';

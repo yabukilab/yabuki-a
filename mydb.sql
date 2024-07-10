d910f5be6313439c42cba27e021da001e1533253
2242120 <s2242120ub@s.chibakoudai.jp>
2024-07-11 02:03:18 +0900

    a

--- mydb.sql
+++ mydb.sql
@@ -119,46 +119,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-07-10 17:19:48
-grant all on mydb.* to testuser@localhost identified by 'pass';
-
--- データベースの使用
-USE mydb;
-
-
--- ユーザ情報テーブルの作成
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
-
--- 書籍情報テーブルの作成
-CREATE TABLE IF NOT EXISTS books (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
-    publisher VARCHAR(255) NOT NULL,
-    user_id INT,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
-
--- データベースの使用
-USE mydb;
-
--- ユーザ情報テーブルの作成
-CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
-
--- 書籍情報テーブルの作成
-CREATE TABLE books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    title VARCHAR(100) NOT NULL,
-    author VARCHAR(100) NOT NULL,
-    publisher VARCHAR(100) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
+-- Dump completed on 2024-07-11  2:03:06

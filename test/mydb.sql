a452b90fe7c842fb91a28a8b8f846f5822e68fb1
sato riku <s2242047pz@s.chibakoudai.jp>
2024-07-11 15:39:35 +0900

    a

---
+++ test/mydb.sql
@@ -0,0 +1,19 @@
+-- データベースの使用
+USE mydb;
+
+-- ユーザ情報テーブルの作成
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+-- 書籍情報テーブルの作成
+CREATE TABLE books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    publisher VARCHAR(100) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);

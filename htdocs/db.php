79c6ee27f594591a1b97f3969f30e34bacf40a47
2242120 <s2242120ub@s.chibakoudai.jp>
2024-07-12 05:14:18 +0900

    a

--- htdocs/db.php
+++ htdocs/db.php
@@ -9,10 +9,10 @@ function h($var) {
   }
 }
 
-$dbServer = isset($_ENV['MYSQL_SERVER'])    ? $_ENV['MYSQL_SERVER']      : '127.0.0.1';
-$dbUser = isset($_SERVER['MYSQL_USER'])     ? $_SERVER['MYSQL_USER']     : 'testuser';
-$dbPass = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : 'pass';
-$dbName = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'mydb';
+$dbServer = '127.0.0.1';  // データベースサーバのホスト名
+$dbUser = 'testuser';      // データベースユーザ名
+$dbPass = 'pass';          // データベースパスワード
+$dbName = 'mydb';          // データベース名
 
 $dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
 
@@ -27,6 +27,7 @@ try {
 } catch (PDOException $e) {
   echo "Can't connect to the database: " . h($e->getMessage());
 }
+?>
 
 
 

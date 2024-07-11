0d7a63764d92da8665b7aad7930178a7d2c9611c
sato riku <s2242047pz@s.chibakoudai.jp>
2024-07-11 17:54:39 +0900

    a

--- mydb.sql
+++ mydb.sql
@@ -6,7 +6,7 @@ USE yabukia;
 
 -- ユーザ情報テーブルの作成
 CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT(11) AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL
 );

f7f90e41814ca164bcbc638491794e5622119a23
sato riku <s2242047pz@s.chibakoudai.jp>
2024-07-11 18:54:05 +0900

    a

--- htdocs/login.php
+++ htdocs/login.php
@@ -8,12 +8,13 @@ function h($var) {
     }
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') 
-    $dbServer = isset($_ENV['MYSQL_SERVER'])    ? $_ENV['MYSQL_SERVER']      : '127.0.0.1';
-    $dbUser = isset($_SERVER['MYSQL_USER'])     ? $_SERVER['MYSQL_USER']     : 'root';
-    $dbPass = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : '';
-    $dbName = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'mydb';
-
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $dbServer = isset($_ENV['MYSQL_SERVER'])    ? $_ENV['MYSQL_SERVER']      : '127.0.0.1';
+        $dbUser = isset($_SERVER['MYSQL_USER'])     ? $_SERVER['MYSQL_USER']     : 'root';
+        $dbPass = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : '';
+        $dbName = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'mydb';
+    }
+    
 $dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
 
 try {

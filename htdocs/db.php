911fa87995b916612c36d6fd23a3285477ee5ccc
2242120 <s2242120ub@s.chibakoudai.jp>
2024-07-12 05:16:14 +0900

    a

--- htdocs/db.php
+++ htdocs/db.php
@@ -1,5 +1,4 @@
 <?php
-
 # HTMLでのエスケープ処理をする関数（データベースとは無関係だが，ついでにここで定義しておく．）
 function h($var) {
   if (is_array($var)) {
@@ -9,23 +8,20 @@ function h($var) {
   }
 }
 
-$dbServer = '127.0.0.1';  // データベースサーバのホスト名
-$dbUser = 'testuser';      // データベースユーザ名
-$dbPass = 'pass';          // データベースパスワード
-$dbName = 'mydb';          // データベース名
+$dbServer = '127.0.0.1';
+$dbUser = 'testuser';
+$dbPass = 'pass';
+$dbName = 'mydb';
 
 $dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
 
-echo "Connecting to database: " . h($dbName); // デバッグ用に追加
+echo "Connecting to database: " . h($dbName) . "<br>"; // デバッグ用に追加
 
 try {
   $db = new PDO($dsn, $dbUser, $dbPass);
-  # プリペアドステートメントのエミュレーションを無効にする．
-  $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-  # エラー→例外
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully to database: " . h($dbName) . "<br>"; // デバッグ用に追加
 } catch (PDOException $e) {
-  echo "Can't connect to the database: " . h($e->getMessage());
+  echo "Can't connect to the database: " . h($e->getMessage()) . "<br>";
 }
 ?>
 

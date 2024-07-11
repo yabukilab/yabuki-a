17cf15283b8b686c86cabb278ed2e8f372685c5b
2242120 <s2242120ub@s.chibakoudai.jp>
2024-07-11 19:51:15 +0900

    a

--- htdocs/add_user.php
+++ htdocs/add_user.php
@@ -1,29 +1,5 @@
 <?php
-session_start();
-
-function h($var) {
-    if (is_array($var)) {
-        return array_map('h', $var);
-    } else {
-        return htmlspecialchars($var, ENT_QUOTES, 'UTF-8');
-    }
-}
-
-$dbServer = isset($_ENV['MYSQL_SERVER'])    ? $_ENV['MYSQL_SERVER']      : '127.0.0.1';
-$dbUser = isset($_SERVER['MYSQL_USER'])     ? $_SERVER['MYSQL_USER']     : 'root';
-$dbPass = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : '';
-$dbName = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'mydb';
-$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
-
-try {
-    $db = new PDO($dsn, $dbUser, $dbPass);
-    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Can't connect to the database: " . h($e->getMessage());
-    exit();
-}
-
+require 'db.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'] ?? '';
     $password = $_POST['password'] ?? '';

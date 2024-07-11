62f7a1b95e0634822e57676252ccbe486eb4384b
sato riku <s2242047pz@s.chibakoudai.jp>
2024-07-11 16:27:16 +0900

    a

--- htdocs/add_user.php
+++ htdocs/add_user.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+# HTMLでのエスケープ処理をする関数
 function h($var) {
     if (is_array($var)) {
         return array_map('h', $var);
@@ -9,16 +10,18 @@ function h($var) {
     }
 }
 
-$dbServer = '127.0.0.1';
-$dbUser = 'testuser';
-$dbPass = 'pass';
-$dbName = 'yabukia';
+$dbServer = isset($_ENV['MYSQL_SERVER'])    ? $_ENV['MYSQL_SERVER']      : '127.0.0.1';
+$dbUser = isset($_SERVER['MYSQL_USER'])     ? $_SERVER['MYSQL_USER']     : 'testuser';
+$dbPass = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : 'pass';
+$dbName = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'yabukia';
 
 $dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
 
 try {
     $db = new PDO($dsn, $dbUser, $dbPass);
+    # プリペアドステートメントのエミュレーションを無効にする
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    # エラー→例外
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Can't connect to the database: " . h($e->getMessage());

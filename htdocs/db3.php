d556eeeb123fbf3ad3eb8f95e0e44741af6b28ad
2242120 <s2242120ub@s.chibakoudai.jp>
2024-07-12 00:27:54 +0900

    a

--- htdocs/db3.php
+++ htdocs/db3.php
@@ -1,20 +1,5 @@
 <?php
-// HTMLでのエスケープ処理をする関数
-function h($var) {
-    if (is_array($var)) {
-        return array_map('h', $var);
-    } else {
-        return htmlspecialchars($var, ENT_QUOTES, 'UTF-8');
-    }
-}
-
-$dbServer = '127.0.0.1';
-$dbUser = 'root';  // ユーザー名
-$dbPass = '';      // パスワード
-$dbName = 'mydb';      // データベース名
-
-$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8mb4";
-
+require"db.php";
 // PDOオブジェクトの作成
 $db = new PDO($dsn, $dbUser, $dbPass);
 # プリペアドステートメントのエミュレーションを無効にする．

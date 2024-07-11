fab0e7875ec92fe5281ad34bfc11ec56d8383f17
2242120 <s2242120ub@s.chibakoudai.jp>
2024-07-11 22:57:25 +0900

    a

--- htdocs/db3.php
+++ htdocs/db3.php
@@ -1,5 +1,20 @@
 <?php
-require "db.php"
+// HTMLでのエスケープ処理をする関数
+function h($var) {
+    if (is_array($var)) {
+        return array_map('h', $var);
+    } else {
+        return htmlspecialchars($var, ENT_QUOTES, 'UTF-8');
+    }
+}
+
+$dbServer = '127.0.0.1';
+$dbUser = 'testuser';  // ユーザー名
+$dbPass = 'pass';      // パスワード
+$dbName = 'mydb';      // データベース名
+
+$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8mb4";
+
 try {
     // PDOオブジェクトの作成
     $db = new PDO($dsn, $dbUser, $dbPass);

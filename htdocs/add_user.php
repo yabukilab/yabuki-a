b39caaa359ad3c1e8019004c6376e81b23e0afcb
2242120 <s2242120ub@s.chibakoudai.jp>
2024-07-11 18:08:12 +0900

    a

--- htdocs/add_user.php
+++ htdocs/add_user.php
@@ -47,9 +47,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
-<!-- HTMLフォーム -->
-<form method="post" action="add_user.php">
-    Username: <input type="text" name="username">
-    Password: <input type="password" name="password">
-    <input type="submit" value="Add User">
-</form>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ユーザ追加画面</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <div class="header"></div>
+    <h1>ユーザ追加画面</h1>
+    <form action="add_user.php" method="post">
+        <label for="username">ユーザ名:</label><br>
+        <input type="text" id="username" name="username" required><br><br>
+        <label for="password">パスワード:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
+        <input type="submit" value="ユーザ追加" class="btn btn--orange">
+    </form>
+</body>
+</html>

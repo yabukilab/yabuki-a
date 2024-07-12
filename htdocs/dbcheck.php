ff52cb7fb1d090527a89fde7fde82e4e47bddd14
sato riku <s2242047pz@s.chibakoudai.jp>
2024-07-12 09:36:10 +0900

    a

---
+++ htdocs/dbcheck.php
@@ -0,0 +1,6 @@
+<?php
+echo "MYSQL_SERVER: " . h($_ENV['MYSQL_SERVER']) . "<br>";
+echo "MYSQL_USER: " . h($_SERVER['MYSQL_USER']) . "<br>";
+echo "MYSQL_PASSWORD: " . h($_SERVER['MYSQL_PASSWORD']) . "<br>";
+echo "MYSQL_DB: " . h($_SERVER['MYSQL_DB']) . "<br>";
+?>

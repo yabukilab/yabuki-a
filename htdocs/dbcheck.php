9694490fb127cb2ca73c229755ce377fd0e49924
sato riku <s2242047pz@s.chibakoudai.jp>
2024-07-12 09:37:48 +0900

    a

--- htdocs/dbcheck.php
+++ htdocs/dbcheck.php
@@ -1,4 +1,14 @@
 <?php
+// HTMLでのエスケープ処理をする関数を定義
+function h($var) {
+  if (is_array($var)) {
+    return array_map('h', $var);
+  } else {
+    return htmlspecialchars($var, ENT_QUOTES, 'UTF-8');
+  }
+}
+
+// 環境変数の値を表示
 echo "MYSQL_SERVER: " . h($_ENV['MYSQL_SERVER']) . "<br>";
 echo "MYSQL_USER: " . h($_SERVER['MYSQL_USER']) . "<br>";
 echo "MYSQL_PASSWORD: " . h($_SERVER['MYSQL_PASSWORD']) . "<br>";

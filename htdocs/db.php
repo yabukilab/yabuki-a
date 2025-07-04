35960c240ca43da3e4a1a2a1070b24b7dfd57859
kogurekazuya <s2342052@s.chibakoudai.jp>
2025-07-04 04:40:10 +0000

    dbお試し

--- htdocs/db.php
+++ htdocs/db.php
@@ -13,7 +13,7 @@ function h($var) {
 $dbServer = '127.0.0.1';
 $dbUser   = isset($_SERVER['MYSQL_USER'])     ? $_SERVER['MYSQL_USER']     : 'testuser';
 $dbPass   = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : 'pass';
-$dbName   = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'mydb';
+$dbName   = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'mydb0';
 
 $dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
 

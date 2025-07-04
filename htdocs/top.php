39b5f4cf224beebd262a68f4b3f0d4274330045f
kogurekazuya <s2342052@s.chibakoudai.jp>
2025-07-04 07:06:26 +0000

    topページ追加

--- htdocs/top.php
+++ htdocs/top.php
@@ -2,13 +2,17 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>講義一覧 - 履修お助けいじばん</title>
-    <link rel="stylesheet" href="a.css">
+    <title>履修お助けいじばん</title>
+    <link rel="stylesheet" href="style2.css">
 </head>
 <body>
+    <div class="header">履修お助けいじばん</div>
+    <main>
 
-
-<a href="lecture.php">一覧</a>
-<a href="ranking.php">ランキング</a>
+        <div class="menu-box">
+<a href="lecture.php" class="menu-button">講義一覧</a>
+<a href="ranking.php" class="menu-button">評価ランキング</a>
+        </div>
+    </main>
 </body>
 </html>

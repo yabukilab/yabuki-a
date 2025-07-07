746dc31510984cce22ee9e2e529d49f649ed31ec
kogurekazuya <s2342052@s.chibakoudai.jp>
2025-07-07 12:25:29 +0000

    db変更

--- htdocs/db.php
+++ htdocs/db.php
@@ -17,11 +17,10 @@ $dbName = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'mydb
 $dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
 
 try {
-  $db = new PDO($dsn, $dbUser, $dbPass);
-  # プリペアドステートメントのエミュレーションを無効にする．
-  $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-  # エラー→例外
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo = new PDO($dsn, $dbUser, $dbPass); // ← ここを $pdo に
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   echo "Can't connect to the database: " . h($e->getMessage());
+  exit;
 }

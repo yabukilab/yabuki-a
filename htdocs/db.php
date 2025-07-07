074914b044a43a470f144634b4fa5f6a6298419f
kogurekazuya <s2342052@s.chibakoudai.jp>
2025-07-07 12:09:23 +0000

    db直し

--- htdocs/db.php
+++ htdocs/db.php
@@ -13,9 +13,7 @@ function h($var) {
 $dbServer = '127.0.0.1';
 $dbUser   = isset($_SERVER['MYSQL_USER'])     ? $_SERVER['MYSQL_USER']     : 'testuser';
 $dbPass   = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : 'pass';
-// 一旦環境変数を無視して強制的にmydbにする
-$dbName = 'mydb';
-
+$dbName   = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'mydb';
 
 $dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
 
